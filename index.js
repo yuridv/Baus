24fdf6191660bbfc908df29f3088bdfccb9bac6d
Yuri Eduardo <57859317+yuridv@users.noreply.github.com>
2024-02-29 15:47:45 -0300

    up

--- index.js
+++ index.js
@@ -8,14 +8,10 @@ client.on('ready', async () => {
 client.login("Njk0NjY5MzI1ODUxNjg4OTkw.Gz_0RQ.-DAfU-kK6jGfWo0wnwNVO-_EUraeWxipy5Zsl0");
 
 client.on('message', async (message) => {
-  if (message.author.id == '1110536642273497129' & message.channelId == '1157118228481454231') {
-    if (message.embeds && message.embeds[0] && message.embeds[0].description) {
-      if (message.embeds[0].description.split('`')[1]) {
-	await Timeout(1500)
-        message.channel.sendSlash('1110536642273497129', 'abrirbau', message.embeds[0].description.split('`')[1])
-      }
+  if (message.author.id == '1110536642273497129' && message.embeds && message.embeds[0] && message.embeds[0].description) {
+    let code = message.embeds[0].description.split('`')[1]
+    if (code) {
+      message.channel.sendSlash('1110536642273497129', 'abrirbau', code)
     }
   }
 });
-
-const Timeout = async (ms) => new Promise(res => setTimeout(res, ms))
