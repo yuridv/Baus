b29718985a7a3cee425982e5d83f10ceb5b27d60
Yuri Eduardo <57859317+yuridv@users.noreply.github.com>
2024-03-03 15:42:35 -0300

    up

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+  console.log(`[SelfBot]=> Starting...`);
+  require('dotenv-safe').config();
+
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client();
 
 client.on('ready', async () => {
-  console.log(`${client.user.username} is ready!`);
+  console.log(`[SelfBot]=> Connected to ${client.user.username}`);
 })
 
-client.login();
+client.login(process.env.TOKEN);
 
 client.on('message', async (message) => {
   if (message.author.id == '1110536642273497129' && message.embeds && message.embeds[0] && message.embeds[0].description) {
     let code = message.embeds[0].description.split('`')[1]
     if (code) {
-      await Timeout(10000)
+      await Timeout(process.env.TIMEOUT)
       message.channel.sendSlash('1110536642273497129', 'abrirbau', code)
     }
   }
